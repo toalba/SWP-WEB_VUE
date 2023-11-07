33905a34ffcb1c941c6a59735be5f3598b077ef4
toalba <tb@kleinundpartner.at>
2023-11-07 15:55:28 +0100

    initial vue project

---
+++ example_vue_hangman/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+// import components here
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+        Hello HelloWorld
+    </div>
+  </header>
+
+  <main>
+      this is vue
+  </main>
+</template>

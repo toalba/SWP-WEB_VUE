08a75116c1514c30ec22056ed360ebbf236f5e79
toalba <tb@kleinundpartner.at>
2023-11-07 17:09:58 +0100

    add usechar mehtode

--- example_vue_hangman/src/components/wordlist.vue
+++ example_vue_hangman/src/components/wordlist.vue
@@ -7,15 +7,30 @@ export default{
         return{
             WordList: words,
             image: 'https://raw.githubusercontent.com/jlbooker/hangman/master/img/hang0.gif',
-            letters : Array.from(Array(26)).map((e, i) => String.fromCharCode(i + 65))
+            letters : Array.from(Array(26)).map((e, i) => String.fromCharCode(i + 65)),
+            word: ''
         }
     },
     methods: {
         getWord(){
-            return this.WordList[Math.floor(Math.random() * this.WordList.length)]
+            // pick a word
+            this.word = this.WordList[Math.floor(Math.random() * this.WordList.length)];
+            console.log(this.word)
+            return this.word;
         },
         showBlanks(word){
-            return word.replace(/\S/g, '_')
+            return word.replace(/\S/g, '_ ');
+        },
+        usechar(e){
+            let char = e.target.outerText.toLowerCase();
+            //check if char exists in word
+            var indexes = []
+            while (this.word.indexOf(char,indexes[-1]) > -1) {
+                indexes.push(this.word.indexOf(char,indexes[-1]))
+            }
+            // delete button
+            this.word.indexOf(char)
+            console.log(indexes);
         }
     }
 }
@@ -26,6 +41,6 @@ export default{
         <h2>Word: {{ showBlanks(getWord()) }}</h2>
         <img :src="image" alt="hangman" />
         <div class="btn-group" role="group"></div>
-        <button type="button" class="btn btn-primary" v-for="letter in letters">{{ letter }}</button>
+        <button :id="letter" type="button" class="btn btn-primary" @click="usechar" v-for="letter in letters">{{ letter }}</button>
     </div>
 </template>

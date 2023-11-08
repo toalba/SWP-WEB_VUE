0482673667d055dc9030110b90e504234ddcc6a1
toalba <tb@kleinundpartner.at>
2023-11-08 15:10:48 +0100

    use timer

--- example_vue_hangman/src/components/wordlist.vue
+++ example_vue_hangman/src/components/wordlist.vue
@@ -1,4 +1,5 @@
 <script>
+import { setTransitionHooks } from 'vue';
 import words from '../assets/wordlist.json'
 export default{
     name: 'wordlist',
@@ -8,29 +9,88 @@ export default{
             WordList: words,
             image: 'https://raw.githubusercontent.com/jlbooker/hangman/master/img/hang0.gif',
             letters : Array.from(Array(26)).map((e, i) => String.fromCharCode(i + 65)),
-            word: ''
+            word: '',
+            hiddenword: '',
+            highscorelist: [],
+            timertxt: ''
         }
     },
+    mounted(){
+        this.starttime = Date.now()
+        this.highscorelist = JSON.parse(localStorage.getItem('highscorelist'));
+        this.start();
+        this.showBlanks();
+        this.redraw();
+        setInterval(this.redraw, 1000);
+    },
+    watch:{
+        highscorelist(newhighscore)
+        {
+            localStorage.setItem('highscorelist', JSON.stringify(newhighscore));
+        },
+    },
     methods: {
-        getWord(){
+        start(){
             // pick a word
             this.word = this.WordList[Math.floor(Math.random() * this.WordList.length)];
             console.log(this.word)
-            return this.word;
         },
-        showBlanks(word){
-            return word.replace(/\S/g, '_ ');
+        showBlanks(){
+            this.hiddenword = this.word.replace(/\S/g, '_ ');
         },
         usechar(e){
             let char = e.target.outerText.toLowerCase();
             //check if char exists in word
             var indexes = []
-            while (this.word.indexOf(char,indexes[-1]) > -1) {
-                indexes.push(this.word.indexOf(char,indexes[-1]))
+            function checkindex(solvingchar, lastindex, word) {
+                let index = word.indexOf(solvingchar,lastindex);
+                if (index > -1) {
+                    indexes.push(index);
+                    checkindex(solvingchar, index+1, word);
+                }
             }
-            // delete button
-            this.word.indexOf(char)
-            console.log(indexes);
+            checkindex(char,0,this.word);
+            if (indexes.length > 0) {
+                let str = this.hiddenword.split(' ');
+                indexes.forEach(index => {
+                    str.splice(index, 1, char);
+                    //this.letters.splice(this.letters.indexOf(char),1)
+                });
+                str = str.join(' ');
+                this.hiddenword = str;
+                if (this.hiddenword.replace(/\s/g, '') == this.word) {
+                    alert('You win!');
+                    this.getWord();
+                    this.showBlanks();
+                    this.image = 'https://raw.githubusercontent.com/jlbooker/hangman/master/img/hang0.gif';
+                    return;
+                }
+            }
+            else
+            {
+                let counterposition = this.image.length - 5;
+                let currentstage = parseInt(this.image.charAt(counterposition));
+                if (currentstage >= 6)
+                {
+                    alert('You Lost');
+                    this.getWord();
+                    this.showBlanks();
+                    this.image = 'https://raw.githubusercontent.com/jlbooker/hangman/master/img/hang0.gif';
+                    return;
+                }
+                let str = this.image.split('');
+                str.splice(counterposition, 1, currentstage + 1);
+                str = str.join('');
+                this.image = str;
+            }
+
+        },
+        redraw()
+        {
+            let seconds=Math.floor((Date.now()-this.starttime)/1000);
+            let minutes=Math.floor(seconds/60);
+            seconds%=60;
+            this.timertxt = minutes.toString().padStart(2,"0")+":"+seconds.toString().padStart(2,"0")
         }
     }
 }
@@ -38,7 +98,8 @@ export default{
 
 <template>
     <div class="container">
-        <h2>Word: {{ showBlanks(getWord()) }}</h2>
+        <h2>Word: {{ this.hiddenword }}</h2>
+        <h3>{{ this.timertxt }}</h3>
         <img :src="image" alt="hangman" />
         <div class="btn-group" role="group"></div>
         <button :id="letter" type="button" class="btn btn-primary" @click="usechar" v-for="letter in letters">{{ letter }}</button>

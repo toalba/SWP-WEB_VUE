05e84a23311e289d9c4c2275f0cd67db3d237876
toalba <tb@kleinundpartner.at>
2024-01-30 16:18:01 +0100

    build a vue component for dark themed plots

---
+++ example_vue_olympia/src/components/darktheme_chart.vue
@@ -0,0 +1,47 @@
+<script>
+import {VuePlotly} from 'vue3-plotly';
+export default {
+    name: 'PlotlyDark',
+    components: {
+        VuePlotly
+    },
+    props: {
+        data: {
+            type: Array,
+            required: true
+        },
+        width: {
+            type: Number,
+            default: 720
+        },
+        height: {
+            type: Number,
+            default: 720
+        },
+        title: {
+            type: String,
+            default: ''
+        },
+    },
+    data() {
+        return {
+            data: this.data,
+            layout: {
+                width: this.width,
+                height: this.height,
+                title: this.title,
+                showlegend: true,
+                paper_bgcolor: '#343a40',
+                plot_bgcolor: '#212529',
+                font: {
+                    family: 'Courier New, monospace',
+                    color: '#dee2e6'
+                }
+            },
+        }
+    }
+}
+</script>
+<template>
+<VuePlotly :data="data" :layout="layout"></VuePlotly>
+</template>
